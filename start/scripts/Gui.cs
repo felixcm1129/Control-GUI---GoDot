2042c2bb65a97a603f72960b3d7e0d37a9d3e122
Félix Carle-Milette <felixcm1129@hotmail.ca>
2020-10-15 23:03:30 -0400

    Animation lost health

--- start/scripts/Gui.cs
+++ start/scripts/Gui.cs
@@ -3,14 +3,36 @@ using System;
 
 public class Gui : MarginContainer
 {
-    private Label _numberLabel;
-    private TextureProgress _bar;
-    private Tween _tween;
+	private Label _numberLabel;
+	private TextureProgress _bar;
+	private Tween _tween;
 
-    public override void _Ready()
-    {
-        _bar = (TextureProgress) GetNode("Bars/LifeBar/TextureProgress");
-        _tween = (Tween) GetNode("Tween");
-        _numberLabel = (Label) GetNode("Bars/LifeBar/Count/Background/Number");
-    }
+	public override void _Ready()
+	{
+		_bar = (TextureProgress) GetNode("Bars/LifeBar/TextureProgress");
+		_tween = (Tween) GetNode("Tween");
+		_numberLabel = (Label) GetNode("Bars/LifeBar/Count/Background/Number");
+		
+		var player = (Player) GetNode("../Characters/Player");
+		_bar.MaxValue = player.MaxHealth;
+		
+		
+	}
+	
+	private void OnPlayerHealthChanged(int playerHealth)
+	{
+		UpdateHealth(playerHealth);
+	}
+	
+	public void UpdateHealth(int health){
+		_numberLabel.Text = health.ToString();
+		_bar.Value = health;
+	}
+	
 }
+
+
+
+
+
+
--- start/scripts/Player.cs
+++ start/scripts/Player.cs
@@ -3,54 +3,54 @@ using System;
 
 public class Player : Node2D
 {
-    [Signal]
-    public delegate void HealthChanged(int health);
+	[Signal]
+	public delegate void HealthChanged(int health);
 
-    [Signal]
-    public delegate void Died();
+	[Signal]
+	public delegate void Died();
 
-    [Export]
-    public int MaxHealth
-    {
-        get { return _health; }
-        set { _health = value; }
-    }
+	[Export]
+	public int MaxHealth
+	{
+		get { return _health; }
+		set { _health = value; }
+	}
 
-    public enum States {
-        Alive,
-        Dead
-    };
+	public enum States {
+		Alive,
+		Dead
+	};
 
-    private States _state = States.Alive;
+	private States _state = States.Alive;
 
-    private int _health = 18;
+	private int _health = 18;
 
-    public void TakeDamage(int count)
-    {
-        if (_state == States.Dead) {
-            return;
-        }
+	public void TakeDamage(int count)
+	{
+		if (_state == States.Dead) {
+			return;
+		}
 
-        _health -= count;
-        if (_health <= 0) {
-            _health = 0;
-            _state = States.Dead;
-            EmitSignal("Died");
-        }
+		_health -= count;
+		if (_health <= 0) {
+			_health = 0;
+			_state = States.Dead;
+			EmitSignal("Died");
+		}
 
-        var animationPlayer = (AnimationPlayer) GetNode("AnimationPlayer");
-        animationPlayer.Play("take_hit");
+		var animationPlayer = (AnimationPlayer) GetNode("AnimationPlayer");
+		animationPlayer.Play("take_hit");
 
-        EmitSignal("HealthChanged", _health);
-    }
+		EmitSignal("HealthChanged", _health);
+	}
 
-    public void OnAnimationPlayerAnimationFinished(string name)
-    {
-        if (_state != States.Dead || name != "take_hit") {
-            return;
-        }
+	public void OnAnimationPlayerAnimationFinished(string name)
+	{
+		if (_state != States.Dead || name != "take_hit") {
+			return;
+		}
 
-        var animationPlayer = (AnimationPlayer) GetNode("AnimationPlayer");
-        animationPlayer.Play("die");
-    }
+		var animationPlayer = (AnimationPlayer) GetNode("AnimationPlayer");
+		animationPlayer.Play("die");
+	}
 }
